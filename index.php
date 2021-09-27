47e5d7e1c67db40b42d06c24fa8d3bae0dbafe5d
Ben <benjamindhayden@gmail.com>
2021-09-27 18:34:53 +0100

    basic styling allowing friendly display of cards. ready for peer review.

--- index.php
+++ index.php
@@ -3,4 +3,28 @@
 
 // print_r(testQuery(1010));
 // var_dump(returnTop50());
-displayCards();
+?>
+<html>
+    <head>
+        <title>MTG Collection</title>
+        <link rel='stylesheet' href='css/normalize.css'>
+        <link rel='stylesheet' href='css/style.css'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    </head>
+    <body>
+    <header>
+    <h1>Magic: The Gathering Collection</h1>
+    </header>
+    <main>
+        <div class="card-container">
+            <?php displayCards(); ?>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+    </body>
+</html>
--- php/db.php
+++ php/db.php
@@ -35,21 +35,24 @@
         $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $query = $db->prepare("
             SELECT `name`, `image_uris.art_crop`, `mana_cost`, `cmc`, `type_line`, `colors`, `set_name`, `rarity`, `prices.usd`, `artist`
-            FROM `mtgcards` 
-            LIMIT 10;"
+            FROM `mtgcards`;"
         );
         $query->execute();
         $result = $query->fetchAll();
         foreach($result as $card) {
-                echo '<h2>' . $card['name'] . "</h2>\n";
-                echo '<img src="' . $card['image_uris.art_crop'] . '">' . "\n";
-                echo '<p>Mana cost: ' . $card['mana_cost'] . "</p>\n";
-                echo '<p>Converted mana cost: ' . $card['cmc'] . "</p>\n";
-                echo '<p>Type: ' . $card['type_line'] . "</p>\n";
-                echo '<p>Colours: ' . $card['colors'] . "</p>\n";
-                echo '<p>Set: ' . $card['set_name'] . "</p>\n";
-                echo '<p>Rarity: ' . $card['rarity'] . "</p>\n";
-                echo '<p>Price: $' . $card['prices.usd'] . "</p>\n";
-                echo '<p>Artist: ' . $card['artist'] . "</p>\n";
+                echo '<div class="card-item">';
+                echo '<div class="card-header"><h3>' . $card['name'] . "</h3>\n";
+                echo '<img src="' . $card['image_uris.art_crop'] . '"></div>' . "\n";
+                echo '<div class="card-stats">';
+                echo '<p><strong>Mana cost</strong>: ' . $card['mana_cost'] . "</p>\n";
+                echo '<p><strong>Converted mana cost</strong>: ' . $card['cmc'] . "</p>\n";
+                echo '<p><strong>Type</strong>: ' . $card['type_line'] . "</p>\n";
+                echo '<p><strong>Colours</strong>: ' . $card['colors'] . "</p>\n";
+                echo '<p><strong>Set</strong>: ' . $card['set_name'] . "</p>\n";
+                echo '<p><strong>Rarity</strong>: ' . $card['rarity'] . "</p>\n";
+                echo '<p><strong>Price</strong>: $' . $card['prices.usd'] . "</p>\n";
+                echo '<p><strong>Artist</strong>: ' . $card['artist'] . "</p>\n";
+                echo '</div>';
+                echo '</div>';
             }
     }
